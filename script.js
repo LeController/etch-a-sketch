91565c0507f5e931b2a95b8487be5dbb9af959ce
Anuj Gupta <anujgupta@windowslive.com>
2022-05-07 00:12:45 +0700

    working etch-a-sketch

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 const container = document.querySelector('#container')
-
 let grid_size;
 
-do {
-    grid_size = prompt("What's the grid size?");
-    console.log(grid_size)
-} while (grid_size > 100);
+gridButton = document.querySelector('#grid-size')
+// gridButton.addEventListener('click', () => {
+//     grid_size = prompt("What's the grid size? (LESS THAN 100)");
+// })
+
+gridButton.addEventListener('click', getGrid)
 
 
-for (let i = 1; i <= grid_size*grid_size ; i++) {
-    const div =  document.createElement('div')
-    div.classList.add("square")
-    div.classList.add(`${i}`)  
-    container.setAttribute('style', `grid-template-columns: repeat(${grid_size}, 1fr);`)   
-    container.appendChild(div);
+function getGrid() {
+    do {
+        grid_size = prompt("What's the grid size? (LESS THAN 100)");
+        console.log(grid_size)
+    } while (grid_size > 100);
+
+    removeAllChildNodes(container);
+
+    for (let i = 1; i <= grid_size*grid_size ; i++) {
+        const div =  document.createElement('div')
+        div.classList.add("square")
+        div.classList.add(`${i}`)  
+        container.setAttribute('style', `grid-template-columns: repeat(${grid_size}, 1fr);`)   
+        container.appendChild(div);
+    }
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
 
 const squares = document.querySelectorAll('.square');
-
 squares.forEach((square) => {
     
     square.addEventListener('mouseover', () => {
