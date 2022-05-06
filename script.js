666ad295815f934e6f0c56a9bd769139fa28833c
Anuj Gupta <anujgupta@windowslive.com>
2022-05-06 21:50:32 +0700

    working etch-a-sketch

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 const container = document.querySelector('#container')
 
-for (let i = 1; i <= 16*16 ; i++) {
+let grid_size;
+
+do {
+    grid_size = prompt("What's the grid size?");
+    console.log(grid_size)
+} while (grid_size > 100);
+
+
+for (let i = 1; i <= grid_size*grid_size ; i++) {
     const div =  document.createElement('div')
     div.classList.add("square")
-    div.classList.add(`${i}`)     
+    div.classList.add(`${i}`)  
+    container.setAttribute('style', `grid-template-columns: repeat(${grid_size}, 1fr);`)   
     container.appendChild(div);
 }
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach((square) => {
+    
+    square.addEventListener('mouseover', () => {
+        square.classList.add('on');
+    });
+});
+
+
