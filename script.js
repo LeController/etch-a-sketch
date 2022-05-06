91cbf5d54b47ec956a29bab3c684d35b68ebdc60
Anuj Gupta <anujgupta@windowslive.com>
2022-05-06 19:43:12 +0700

    change to css grid

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 const container = document.querySelector('#container')
 
-for (let i = 1; i <= 16 ; i++) {
+for (let i = 1; i <= 16*16 ; i++) {
     const div =  document.createElement('div')
-    div.classList.add("column")
-
+    div.classList.add("square")
+    div.classList.add(`${i}`)     
     container.appendChild(div);
-
-    for (let j = 1; j <= 16; j++) {
-        const div2 =  document.createElement('div')
-        div2.classList.add("square")
-        div2.classList.add(`${i}.${j}`)
-        div.appendChild(div2);
-    }
 }
